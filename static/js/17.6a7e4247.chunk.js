486c91942553155ec9ca837baba1deb68459d41a
Drummberg <drummberg@gmail.com>
2022-07-01 02:38:04 +0300

    Updates

--- static/js/17.6a7e4247.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[17],{17:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"HomePage"})}}}]);
-//# sourceMappingURL=17.6a7e4247.chunk.js.map
